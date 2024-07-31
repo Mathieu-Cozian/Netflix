395863ba8ee4783d351bc0107f31fa3d9507d5b5
Mathieu-Cozian <mathieu.cozian@ieseg.fr>
2024-07-31 18:14:34 +0200

    Pushing to git

---
+++ db/migrate/20240731160505_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.date :release_date
+      t.float :rating
+      t.string :genre
+      t.timestamps
+    end
+  end
+end
